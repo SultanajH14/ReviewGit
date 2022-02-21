0ad73ac89967c012cc5d150f30cbd5de811c6f02
unknown <sultana.jarfin@gmail.com>
2022-02-21 13:53:40 -0500

    added targeturl

--- Testngrew/src/main/java/com/bit/recou/UtilityTest.java
+++ Testngrew/src/main/java/com/bit/recou/UtilityTest.java
@@ -1,9 +1,11 @@
 package com.bit.recou;
 
+import org.openqa.selenium.WebDriver;
+
 public class UtilityTest {
- 
+ WebDriver dr;
 	
 	public void getAnyUrl() {
-		
+		dr.get("https://www.target.com/");
 	}
 }
